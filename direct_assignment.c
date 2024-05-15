374162655aef156839fc63db16ab37c212066ce3
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-05-15 15:41:02 +0000

    direct assignment - assign value to member variables

---
+++ direct_assignment.c
@@ -0,0 +1,18 @@
+#include "structs.h"
+
+struct	s_movie
+{
+	char	title[100];
+	char	director[50];
+	int		duration_min;
+};
+
+// int	main(void)
+// {
+// 	struct s_movie	t_my_movie;
+
+// 	kai_strcpy(t_my_movie.title, "Harry Potter and the Philosopher's Stone");
+// 	kai_strcpy(t_my_movie.director, "Chris Columbus");
+// 	t_my_movie.duration_min = 159;
+// 	return (0);
+// }
