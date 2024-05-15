08776a537f51cf1a254607625f99b719c1bca68d
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-05-15 13:45:34 +0000

    new beginnings - starting over with Structs in C

--- main.c
+++ main.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 
-typedef struct s_order
+struct	s_book
 {
-	int		id;
-	int		cost;
-	char	desc[100];
-}	t_order;
-
-typedef struct s_customer
-{
-	char	name[50];
-	int		id;
-	char	*order_id;
-}	t_customer;
-
-typedef struct s_shop
-{
-	t_customer	*customers;
-	t_order	*orders;
-}	t_shop;
-
-void	add_order(t_order *new_order, t_shop *shop)
-{
-	
-}
+	char	title[100];
+	char	author[50];
+	int		page;
+};
 
 int	main(void)
 {
+	struct s_book	my_book;
 
 	return (0);
 }
