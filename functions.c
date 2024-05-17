4d3044ffdc1ec8993efab91d0e94dea7c6f82e9f
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-05-17 17:56:43 +0000

    functions - moved strcpy here and deleted original

--- functions.c
+++ functions.c
@@ -16,10 +16,39 @@ void	print_all_movies(struct s_movie movies[])
 	int	i;
 
 	i = 0;
-	while(movies[i].title[i] != '\0')
-		{
-			printf("\nTitle: %s\nDirector: %s\nDuration: %imin.\n",
-				movies[i].title, movies[i].director, movies[i].duration_min);
-			i++;
-		}
+	while (movies[i].title[i] != '\0')
+	{
+		printf("\nTitle: %s\nDirector: %s\nDuration: %imin.\n",
+			movies[i].title, movies[i].director, movies[i].duration_min);
+		i++;
+	}
+}
+
+void	p_print_all_movies(struct s_movie *movies)
+{
+	int	i;
+
+	i = 0;
+	while (movies[i].title[i] != '\0')
+	{
+		printf("\nTitle: %s\nDirector: %s\nDuration: %imin.\n",
+			movies[i].title, movies[i].director, movies[i].duration_min);
+		i++;
+	}
+}
+
+char	*kai_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	if (!src)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
