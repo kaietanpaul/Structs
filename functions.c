ca81e66a5a2efadf9aaff729840880f4dd5e5214
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-05-17 13:36:00 +0000

    functions - added print_all_movies

--- functions.c
+++ functions.c
@@ -2,11 +2,24 @@
 
 void	print_movie(struct s_movie movie)
 {
-	printf("Title: %s\nDirector: %s\nDuration: %imin.\n",
+	printf("\nTitle: %s\nDirector: %s\nDuration: %imin.\n",
 		movie.title, movie.director, movie.duration_min);
 }
 
 void	set_movie_duration(struct s_movie *movie, int new_duration)
 {
 	movie->duration_min = new_duration;
+}
+
+void	print_all_movies(struct s_movie movies[])
+{
+	int	i;
+
+	i = 0;
+	while(movies[i].title[i] != '\0')
+		{
+			printf("\nTitle: %s\nDirector: %s\nDuration: %imin.\n",
+				movies[i].title, movies[i].director, movies[i].duration_min);
+			i++;
+		}
 }
