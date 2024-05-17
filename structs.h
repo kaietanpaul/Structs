5aecfeda1d8b6408a3f78f28c45db4fd42956604
Kaietan <34032203+kaietanpaul@users.noreply.github.com>
2024-05-17 17:56:56 +0000

    updates

--- structs.h
+++ structs.h
@@ -2,15 +2,19 @@
 # define STRUCTS_H
 
 # include <stdio.h>
+# include <stdlib.h>
 struct	s_movie
 {
 	char	title[100];
 	char	director[50];
 	int		duration_min;
 };
-char	*kai_strcpy(char *dest, char *src);
 
-void	print_movie(struct s_movie movie);
-void	set_movie_duration(struct s_movie *movie, int new_duration);
+char	*kai_strcpy(char *, char *);
+
+void	print_movie(struct s_movie);
+void	set_movie_duration(struct s_movie *, int);
+void	print_all_movies(struct s_movie []);
+void	p_print_all_movies(struct s_movie *);
 
 #endif
